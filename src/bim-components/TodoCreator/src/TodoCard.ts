1ecbf693326a8a33ec20f44653abb93ef2cb8b80
jcesarpolo <julio@tailorbird.us>
2024-01-29 18:02:56 -0500

    Feat: add slots to ToDos component

--- src/bim-components/TodoCreator/src/TodoCard.ts
+++ src/bim-components/TodoCreator/src/TodoCard.ts
@@ -3,6 +3,9 @@ import * as OBC from "openbim-components"
 export class TodoCard extends OBC.SimpleUIComponent {
 
     onCardClick = new OBC.Event()
+    slots: { 
+        actionButtons: OBC.SimpleUIComponent
+    }
     
     set description(value: string){
         const descriptionElement = this.getInnerElement("description") as HTMLParagraphElement
@@ -18,14 +21,17 @@ export class TodoCard extends OBC.SimpleUIComponent {
     constructor(components: OBC.Components){
         const template = `
         <div class="todo-item">
-            <div style="display: flex; flex-direction: row; column-gap: 10px; border-radius: 10px; align-items: center;">
+            <div style="display: flex; flex-direction: row; justify-content: space-between; align-items: center; border-radius: 10px;">
                 <div style="display: flex; column-gap: 15px; align-items: center;">
                     <p style="background-color: #979696; aspect-ratio: 1; align-items: center; border-radius: 10px; padding: 8px;"><span class="material-icons-round">
                         construction
                         </span></p>
-                    <p id="description" style="font-size: var(--font-sm);">Make anything here  as you want,even something longer</p>
+                    <div style="display: flex; flex-direction: row">
+                        <p id="date" style="width: 100px; font-size: var(--font-base);">Fri, 20 sep</p>
+                        <p id="description" style="font-size: var(--font-sm);">Make anything here  as you want,even something longer</p>
+                    </div>
                 </div>
-                <p id="date" style="width: 100px; font-size: var(--font-base);">Fri, 20 sep</p>
+                <div data-tooeen-slot="actionButtons"></div>
             </div>
         </div>
         `
@@ -34,5 +40,12 @@ export class TodoCard extends OBC.SimpleUIComponent {
         cardElement.addEventListener("click", () => {
             this.onCardClick.trigger()
         })
+
+        this.setSlot("actionButtons", new OBC.SimpleUIComponent(this.components))
+        
+        const deleteBtn = new OBC.Button(this.components)
+        deleteBtn.materialIcon = "delete"
+
+        this.slots.actionButtons.addChild(deleteBtn)
     }
 }
